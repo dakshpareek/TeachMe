a007015a358cf28d6619cf16da5780609e241221
Daksh Pareek <dakshpareek7@gmail.com>
2020-07-21 13:08:57 +0530

    Project Initialized

---
+++ src/test/java/com/teachme/teachme/TeachmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachme.teachme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
